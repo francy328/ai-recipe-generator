51fe8c82b8fd966298d9c0a3ac7978221058de57
francy328 <fviscomi@gmail.com>
2025-08-29 14:15:06 +0200

    10 commit

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,2 +1,34 @@
-import { type ClientSchema, a, defineData } from "@aws-amplify/backend"; const schema = a.schema({ BedrockResponse: a.customType({ body: a.string(), error: a.string(), }), askBedrock: a.query().arguments({ ingredients: a.string().array() }).returns(a.ref("BedrockResponse")).authorization((allow) =& gt;[allow.authenticated()]).handler(a.handler.custom({ entry: "./bedrock.js", dataSource: "bedrockDS" })), }); export type Schema = ClientSchema;
-export const data = defineData({ schema, authorizationModes: { defaultAuthorizationMode: "apiKey", apiKeyAuthorizationMode: { expiresInDays: 30, }, }, });
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  BedrockResponse: a.customType({
+    body: a.string(),
+    error: a.string(),
+  }),
+
+  askBedrock: a
+    .query()
+    .arguments({
+      ingredients: a.string().array(),
+    })
+    .returns(a.ref("BedrockResponse"))
+    .authorization((allow) => [allow.authenticated()])
+    .handler(
+      a.handler.custom({
+        entry: "./bedrock.js",
+        dataSource: "bedrockDS",
+      })
+    ),
+});
+
+export type Schema = ClientSchema;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
